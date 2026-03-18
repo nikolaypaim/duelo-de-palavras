8fb29cf7febf8b2f5f4511379d35a99394e324e6
Nikolay Paim <nikolaypaim@gmail.com>
2026-03-17 22:01:32 -0300

    adicionando design na game-page.html

--- game-loguin.js
+++ game-loguin.js
@@ -1,8 +1,8 @@
 function addUser(){
     player1Name = document.getElementById("player1-name-input").value;
     player2Name = document.getElementById("player2-name-input").value;
-    localStorage.setItem("player1_name", player1Name); 
-    localStorage.setItem("player2_name", player2Name);
+    localStorage.setItem("player1-name", player1Name); 
+    localStorage.setItem("player2-name", player2Name);
     window.location = "game-page.html";
 
 }
